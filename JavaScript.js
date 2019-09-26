930e5757e6ec408e3b360ace132fc23722d8d479
JanReuvers <48521636+JanReuvers@users.noreply.github.com>
2019-09-26 10:28:17 +0200

    style sheet toegevoegd

--- JavaScript.js
+++ JavaScript.js
@@ -3,6 +3,19 @@
 
 }
 
+function Sqareroot(n) {
+    var x = n;
+    var y = 1;
+    var e = 0.00001;
+
+    while (x - y > e) {
+        x = (x + y) / 2;
+        y = n / x;
+        sqrt()
+    }
+    return (x);
+}
+
 function JavaGetDate() {
     document.getElementById('CurrentTime').innerHTML = Date()
    
@@ -16,5 +29,22 @@ function BeerTime() {
     }
     else {
         document.getElementById('ItIsTime').innerHTML = "<p>Helaas, het is nog geen tijd voor bier. Het is nog te vroeg</p>"
+    }
+}
+
+function collapsible() {
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
     }
 }
