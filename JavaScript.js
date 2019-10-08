e0f5a6d07be00a5129b7ab8e138c40fb46d5083b
JanReuvers <48521636+JanReuvers@users.noreply.github.com>
2019-10-08 12:31:32 +0200

    Week6 start, connection en connection opdracht toegevoegd

--- JavaScript.js
+++ JavaScript.js
@@ -62,12 +62,23 @@ function weatherBalloon(cityID) {
 }
 
 function drawWeather(d) {
+    
     var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
     var fahrenheit = Math.round(((parseFloat(d.main.temp) - 273.15) * 1.8) + 32);
+    var description = d.weather[0].description;
 
-    document.getElementById('description').innerHTML = d.weather[0].description;
+    document.getElementById('description').innerHTML = description;
     document.getElementById('temp').innerHTML = celcius + '&deg;';
     document.getElementById('location').innerHTML = d.name;
+
+    //if (description.indexOf('rain') > 0) {
+    //    document.weatherBody.className = 'rainy';
+    //} else if (description.indexOf('cloud') > 0) {
+    //    document.weatherBody.className = 'cloudy';
+    //} else if (description.indexOf('sunny') > 0) {
+    //    document.weatherBody.className = 'sunny';
+    //}
+    
 }
 
 //window.onload = function () {
