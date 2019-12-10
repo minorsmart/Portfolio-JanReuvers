0bd0187228b98cdd5e16870747a76cfefe840017
JanReuvers <48521636+JanReuvers@users.noreply.github.com>
2019-12-10 15:58:23 +0100

    Dropdown repaired to click 1time

--- JavaScript.js
+++ JavaScript.js
@@ -2,20 +2,25 @@
 
 //Collapsible
 function collapsible() {
-    var coll = document.getElementsByClassName("collapsible");
+    var acc = document.getElementsByClassName("collapsible");
     var i;
 
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function () {
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
             this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
             var content = this.nextElementSibling;
-            if (content.style.maxHeight) {
-                content.style.maxHeight = null;
+            if (content.style.display === "block") {
+                content.style.display = "none";
             } else {
-                content.style.maxHeight = content.scrollHeight + "px";
+                content.style.display = "block";
             }
         });
     }
+
 }
 
 
